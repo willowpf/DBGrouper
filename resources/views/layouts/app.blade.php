9a0def50d3227cb83f3a31092254a2474721022a
Jarcel Franz Tubigon <jarcelfranz@gmail.com>
2025-05-03 02:44:03 +0800

    MIGRATION FIXED TRANSACTION

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'UniqIce') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
